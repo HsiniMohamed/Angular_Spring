50a9078e6ee25a4ddec53746c6d318960ea2d36b
Mohamed Hsini <hssinimohamed5@gmail.com>
2024-03-04 19:53:36 +0100

    Creation 2 beans JWTEncoder And JWTDecoder

--- src/main/java/com/example/digitalBanking/security/SecurityConifg.java
+++ src/main/java/com/example/digitalBanking/security/SecurityConifg.java
@@ -1,5 +1,7 @@
 package com.example.digitalBanking.security;
 
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -9,8 +11,15 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
 
 @Configuration
 @EnableWebSecurity
@@ -36,9 +45,23 @@ public class SecurityConifg {
 				.sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				.authorizeHttpRequests(ar->ar.anyRequest().authenticated())
 				.csrf(csrf->csrf.disable())
-				.httpBasic(Customizer.withDefaults())
+				//.httpBasic(Customizer.withDefaults())
+				.oauth2ResourceServer(oa->oa.jwt(Customizer.withDefaults()))
 				.build();
 		
+	}
+	
+	@Bean
+	JwtEncoder jwtEncoder() {
+		String secretKey="1AZSXC4VWS3EDFR45HYJI765OLMPBV2D2E5TGVBHJUX0586HFGVN54KIRNCY4375HY";
+		return new NimbusJwtEncoder(new ImmutableSecret<>(secretKey.getBytes()));
+	}
+	
+	@Bean 
+	JwtDecoder jwtDecoder() {
+		String secretKey="1AZSXC4VWS3EDFR45HYJI765OLMPBV2D2E5TGVBHJUX0586HFGVN54KIRNCY4375HY";
+		SecretKeySpec secretKeySpec= new SecretKeySpec( secretKey.getBytes(),"RSA");
+		return NimbusJwtDecoder.withSecretKey(secretKeySpec).macAlgorithm(MacAlgorithm.HS512).build();
 	}
 
 }
