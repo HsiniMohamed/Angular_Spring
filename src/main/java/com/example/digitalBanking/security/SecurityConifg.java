edcc42095450b166d29cc2a11e2f1da71395a2c1
Mohamed Hsini <hssinimohamed5@gmail.com>
2024-03-04 19:35:57 +0100

    Creation Filter Chain

--- src/main/java/com/example/digitalBanking/security/SecurityConifg.java
+++ src/main/java/com/example/digitalBanking/security/SecurityConifg.java
@@ -2,11 +2,15 @@ package com.example.digitalBanking.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +28,17 @@ public class SecurityConifg {
 	@Bean
 	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		return httpSecurity
+				.sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+				.csrf(csrf->csrf.disable())
+				.httpBasic(Customizer.withDefaults())
+				.build();
+		
 	}
 
 }
